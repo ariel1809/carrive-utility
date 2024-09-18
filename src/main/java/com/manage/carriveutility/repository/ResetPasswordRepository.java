c03e0dc282326006fb7c0e5a14d69a7ffc37d151
ariel <stevekamga18@gmail.com>
2024-09-18 13:35:33 +0100

    carrive utility

--- src/main/java/com/manage/carriveutility/repository/ResetPasswordRepository.java
+++ src/main/java/com/manage/carriveutility/repository/ResetPasswordRepository.java
@@ -1,6 +1,5 @@
 package com.manage.carriveutility.repository;
 
-import com.manage.carrive.entity.RegisterCode;
 import com.manage.carrive.entity.ResetPassword;
 import com.manage.carrive.entity.UserCarrive;
 import org.springframework.data.mongodb.repository.MongoRepository;
