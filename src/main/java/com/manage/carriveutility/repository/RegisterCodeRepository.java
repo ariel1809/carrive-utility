a37722c843988e3512fc0095ade35bf306ef4804
ariel <stevekamga18@gmail.com>
2024-09-18 09:46:07 +0100

    carrive utility

--- src/main/java/com/manage/carriveutility/repository/RegisterCodeRepository.java
+++ src/main/java/com/manage/carriveutility/repository/RegisterCodeRepository.java
@@ -1,6 +1,7 @@
 package com.manage.carriveutility.repository;
 
 import com.manage.carrive.entity.RegisterCode;
+import com.manage.carrive.entity.UserCarrive;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
@@ -8,4 +9,5 @@ import java.util.Optional;
 public interface RegisterCodeRepository extends MongoRepository<RegisterCode, String> {
     Optional<RegisterCode> findByCode(Integer code);
     Boolean existsByCode(Integer code);
+    Optional<RegisterCode> findByUser(UserCarrive userCarrive);
 }
