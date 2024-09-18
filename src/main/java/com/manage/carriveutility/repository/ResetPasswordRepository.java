f4b75e96f39572e2bc7ceb4dd5af8eb8b3700d34
ariel <stevekamga18@gmail.com>
2024-09-18 10:00:06 +0100

    carrive utility

--- src/main/java/com/manage/carriveutility/repository/ResetPasswordRepository.java
+++ src/main/java/com/manage/carriveutility/repository/ResetPasswordRepository.java
@@ -1,10 +1,16 @@
 package com.manage.carriveutility.repository;
 
+import com.manage.carrive.entity.RegisterCode;
 import com.manage.carrive.entity.ResetPassword;
+import com.manage.carrive.entity.UserCarrive;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ResetPasswordRepository extends MongoRepository<ResetPassword, String> {
+    Optional<ResetPassword> findByCode(Integer code);
     Boolean existsByCode(Integer code);
+    Optional<ResetPassword> findByUser(UserCarrive userCarrive);
 }
