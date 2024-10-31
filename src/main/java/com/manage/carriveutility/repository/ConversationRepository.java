4850ee346af14b9cab08773ae91758421c2c90b3
ariel1809 <81252075+ariel1809@users.noreply.github.com>
2024-10-31 07:22:32 +0100

    update

--- src/main/java/com/manage/carriveutility/repository/ConversationRepository.java
+++ src/main/java/com/manage/carriveutility/repository/ConversationRepository.java
@@ -1,9 +1,17 @@
 package com.manage.carriveutility.repository;
 
 import com.manage.carrive.entity.Conversation;
+import com.manage.carrive.entity.Driver;
+import com.manage.carrive.entity.Passenger;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ConversationRepository extends MongoRepository<Conversation, String> {
+    Optional<Conversation> findByUser1AndUser2(Driver user1, Passenger user2);
+    List<Conversation> findByUser1(Driver user1);
+    List<Conversation> findByUser2(Passenger user2);
 }
--- src/main/java/com/manage/carriveutility/repository/PersonalDocumentRepository.java
+++ src/main/java/com/manage/carriveutility/repository/PersonalDocumentRepository.java
@@ -1,9 +1,13 @@
 package com.manage.carriveutility.repository;
 
+import com.manage.carrive.entity.Driver;
 import com.manage.carrive.entity.PersonalDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PersonalDocumentRepository extends MongoRepository<PersonalDocument, String> {
+    Optional<PersonalDocument> findByDriver(Driver driver);
 }
