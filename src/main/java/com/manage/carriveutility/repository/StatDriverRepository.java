39bd1d36169a6726be2d1a38038df0116077c3ff
ariel <Naruto18@>
2024-11-01 21:29:06 +0100

    stat driver

--- src/main/java/com/manage/carriveutility/repository/StatDriverRepository.java
+++
@@ -1,9 +0,0 @@
-package com.manage.carriveutility.repository;
-
-import com.manage.carrive.entity.StatDriver;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StatDriverRepository extends MongoRepository<StatDriver, String> {
-}
